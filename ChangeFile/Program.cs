c10a53d25437522bab0b3c003e5643c47c72ab59
Zz <Zz@ZZ>
2026-01-10 21:00:32 +0800

    添加项目文件。

---
+++ ChangeFile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:/sers/Zz/Desktop";
+            DirectoryInfo dir = new DirectoryInfo(path);
+            SerialPort port = new SerialPort();
+            Console.WriteLine(SerialPort.GetPortNames());
+            //port.Open();
+            Console.ReadKey();
+        }
+    }
+}
